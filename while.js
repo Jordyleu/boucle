eb3c2dd0265525174733122dd607780f72fe68f3
Gabriel <145458033+Jordyleu@users.noreply.github.com>
2023-09-22 15:53:46 +0200

    Add files via upload

---
+++ while.js
@@ -0,0 +1,13 @@
+"use strict";
+let a=3;
+while(a<9){
+    aT++
+    if(aT==7){
+        continue
+    }
+    if(aT==8){
+        break
+    }
+    console.log(aT);
+}
+console.log('Jai cassé la boucle au bout de '+aT+" tours");
